99c1d5eeef06f2603a58694be7d4a7d6ac807f57
Abdullah Khan <khanabdullahj@gmail.com>
2017-01-03 14:54:43 -0500

    Use std::array instead of raw pointers.

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <array>
 
 #ifdef __APPLE__
 #include <OpenCL/opencl.h>
@@ -14,11 +15,9 @@
 
 using namespace std;
 
-int main(int argc, char * argv[])
+int main()
 {
-
 	// Start timing.
-
 	std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
 
 	cl_context context = 0;
@@ -34,7 +33,7 @@ int main(int argc, char * argv[])
 
 	if(context == NULL)
 	{
-		cerr << "Failed to create OpenCL context." << endl;
+		cerr << "Failed to create OpenCL context.\n";
 
 		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 		timer(start, end);
@@ -67,7 +66,7 @@ int main(int argc, char * argv[])
 	kernel = clCreateKernel(program, "hello_kernel", NULL);
 	if(kernel == NULL)
 	{
-		cerr << "Failed to create kernel." << endl;
+		cerr << "Failed to create kernel." << '\n';
 		Cleanup(context, commandQueue, program, kernel, memObjects);
 
 		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
@@ -77,53 +76,55 @@ int main(int argc, char * argv[])
 
 	// Create memory objects that will be used as arguments to kernel.
 	// First create host memory arrays that will store the arguments to kernel.
-
-	float result[ARRAY_SIZE];
-	float a[ARRAY_SIZE];
-	float b[ARRAY_SIZE];
+	array<float, ARRAY_SIZE> result = {};
+	array<float, ARRAY_SIZE> a = {};
+	array<float, ARRAY_SIZE> b = {};
 
 	// Open two files a.txt and b.txt because they contain the arrays.
-
-	ifstream a_array;
-	ifstream b_array;
-
-	// Open files for reading.
-	a_array.open("a.txt");
-	b_array.open("b.txt");
-
-	if(a_array.good() && b_array.good())
 	{
-		cout << "Reading arrays from files." << endl;
+		// The input file streams will be closed at the end of this scope.
+		ifstream a_array;
+		ifstream b_array;
 
-		string line_a;
-		string line_b;
+		// Open files for reading.
+		a_array.open("a.txt");
+		b_array.open("b.txt");
 
-		int start_a = 0;
-		while(getline(a_array, line_a))
+		if(a_array.good() && b_array.good())
 		{
-			a[start_a] = atof(line_a.c_str());
-			start_a++;
-		}
+			cout << "Reading arrays from files.\n";
 
-		int start_b = 0;
-		while(getline(b_array, line_b))
-		{
-			b[start_b] = atof(line_b.c_str());
-			start_b++;
-		}
+			string line_a;
+			string line_b;
 
-		for(int i = 0; i < ARRAY_SIZE; i++)
-		{
-			//a[i] = (float)i;
-			cout << "a[" << i << "] = " << a[i] << endl;
+			int index = 0;
+			while(getline(a_array, line_a))
+			{
+				a[index] = atof(line_a.c_str());
+				index++;
+			}
 
-			//b[i] = (float)(2*i);
-			cout << "b[" << i << "] = " << b[i] << endl;
-
+			index = 0;
+			while(getline(b_array, line_b))
+			{
+				b[index] = atof(line_b.c_str());
+				index++;
+			}
 		}
 	}
 
-	if(CreateMemObjects(context, memObjects, a, b) == false)
+	// Print out contents of arrays.
+	for(unsigned int i = 0; i < ARRAY_SIZE; i++)
+	{
+		//a[i] = (float)i;
+		cout << "a[" << i << "] = " << a[i] << '\n';
+
+		//b[i] = (float)(2*i);
+		cout << "b[" << i << "] = " << b[i] << '\n';
+
+	}
+
+	if(CreateMemObjects(context, memObjects, a.data(), b.data()) == false)
 	{
 		Cleanup(context, commandQueue, program, kernel, memObjects);
 		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
@@ -134,10 +135,10 @@ int main(int argc, char * argv[])
 	errNum = clSetKernelArg(kernel, 0, sizeof(cl_mem), &memObjects[0]);
 	errNum |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &memObjects[1]);
 	errNum |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &memObjects[2]);
-	
+
 	if (errNum != CL_SUCCESS)
 	{
-		cerr << "Error setting kernel arguments." << endl;
+		cerr << "Error setting kernel arguments.\n";
 		Cleanup(context, commandQueue, program, kernel, memObjects);
 
 		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
@@ -152,7 +153,7 @@ int main(int argc, char * argv[])
 	errNum = clEnqueueNDRangeKernel(commandQueue, kernel, 1, NULL, globalWorkSize, localWorkSize, 0, NULL, NULL);
 	if (errNum != CL_SUCCESS)
 	{
-		cerr << "Error." << endl;
+		cerr << "Error.\n";
 		Cleanup(context, commandQueue, program, kernel, memObjects);
 
 		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
@@ -162,10 +163,10 @@ int main(int argc, char * argv[])
 
 	// Transfer the result from buffer back to host.
 	errNum = clEnqueueReadBuffer(commandQueue, memObjects[2], CL_TRUE, 0, ARRAY_SIZE * sizeof(float),
-								 result, 0, NULL, NULL);
+	result.data(), 0, NULL, NULL);
 	if(errNum != CL_SUCCESS)
 	{
-		cerr << "Error reading result buffer." << endl;
+		cerr << "Error reading result buffer.\n";
 		Cleanup(context, commandQueue, program, kernel, memObjects);
 
 		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
@@ -173,24 +174,22 @@ int main(int argc, char * argv[])
 		return 1;
 	}
 
-	// Output the result buffer
-	// To a file.
+	// Output the result buffer to a file.
 	ofstream result_array;
-
 	result_array.open("results.txt");
-
 	if(result_array.good())
 	{
-		for(int i = 0; i < ARRAY_SIZE; i++)
+		for(unsigned int i = 0; i < ARRAY_SIZE; i++)
 		{
-			result_array << result[i] << endl;
+			result_array << result[i] << '\n';
 		}
-
-		cout << "Results saved to results.txt." << endl;
+		cout << "Results saved to results.txt.\n";
 	}
+	result_array.close();
 
-	cout << endl;
-	cout << "Executed program successfully." << endl;
+
+	cout << '\n';
+	cout << "Executed program successfully.\n";
 	Cleanup(context, commandQueue, program, kernel, memObjects);
 
 	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
