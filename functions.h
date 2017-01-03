35af06edd95317c351aae4727dc3b5d88dfd33aa
Abdullah Khan <khanabdullahj@gmail.com>
2017-01-03 14:46:35 -0500

    Switch include guard for #pragma once.

--- functions.h
+++ functions.h
@@ -1,30 +1,30 @@
 // functions.h
 // HelloWorld
 
-#ifndef __HelloWorld__functions__
-#define __HelloWorld__functions__
+#pragma once
 
+#include <chrono> // To time how long our program takes to run.
+
+// OpenCL headers
 #ifdef __APPLE__
 #include <OpenCL/opencl.h>
 #else
 #include <CL/opencl.h>
 #endif
 
-#include <chrono> // To time how long our program takes to run.
-
-#define ARRAY_SIZE 1000
+const unsigned int ARRAY_SIZE = 100;
 
 cl_context CreateContext();
 cl_command_queue CreateCommandQueue(cl_context, cl_device_id *);
 cl_program CreateProgram(cl_context, cl_device_id, const char *);
 
 bool CreateMemObjects(cl_context, cl_mem memObjects[3], float *, float *);
-void Cleanup(cl_context context, cl_command_queue commandQueue,
-	           cl_program program, cl_kernel kernel,
-						 cl_mem memObjects[3]);
+void Cleanup(cl_context context,
+			cl_command_queue commandQueue,
+			cl_program program, cl_kernel kernel,
+			cl_mem memObjects[3]);
 
 // Returns program duration.
 void timer(std::chrono::steady_clock::time_point start, std::chrono::steady_clock::time_point end);
 
 #include "functions.cpp"
-#endif /* defined(__HelloWorld__functions__) */
